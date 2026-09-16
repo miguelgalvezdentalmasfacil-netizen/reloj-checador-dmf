046a64a691bb69d728631b5e86589e39a2f5d491
Miguel Galvez Ventas <miguel@dentalmasfacil.com>
2026-09-15 17:07:02 -0700

    Añadir Service Worker para habilitar botón de instalación PWA en Android

---
+++ sw.js
@@ -0,0 +1,14 @@
+﻿const CACHE_NAME = 'dmf-pwa-v1';
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    // Modo seguro: Siempre busca en internet para que las entradas del reloj nunca fallen ni se queden guardadas en caché viejo.
+    event.respondWith(fetch(event.request));
+});
